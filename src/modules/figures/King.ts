087aca8372ba65208df14b44b64d391271cc6153
Сергей Гретченко <sergejgretcenko@MacBook-Pro-Vladimir-2.local>
2024-01-25 14:11:17 +0800

    first commit

---
+++ src/modules/figures/King.ts
@@ -0,0 +1,19 @@
+import { Cell } from "../Cell";
+import { Colors } from "../Color";
+import { Figure, FigureNames } from "./Figure";
+import blacklogo from "../../assets/black-king.png";
+import whitelogo from "../../assets/white-king.png";
+
+export class King extends Figure {
+  constructor(color: Colors, cell: Cell) {
+    super(color, cell);
+    this.logo = color === Colors.BLACK ? blacklogo : whitelogo;
+    this.name = FigureNames.KING;
+  }
+  canMove(target: Cell): boolean {
+    if (!super.canMove(target)) {
+      return false;
+    }
+    return true;
+  }
+}
---
+++ src/modules/figures/Rook.ts
@@ -0,0 +1,25 @@
+import { Cell } from "../Cell";
+import { Colors } from "../Color";
+import { Figure, FigureNames } from "./Figure";
+import blacklogo from "../../assets/black-rook.png";
+import whitelogo from "../../assets/white-rook.png";
+
+export class Rook extends Figure {
+  constructor(color: Colors, cell: Cell) {
+    super(color, cell);
+    this.logo = color === Colors.BLACK ? blacklogo : whitelogo;
+    this.name = FigureNames.ROOK;
+  }
+  canMove(target: Cell): boolean {
+    if (!super.canMove(target)) {
+      return false;
+    }
+    if (this.cell.isEmptyVertical(target)) {
+      return true;
+    }
+    if (this.cell.isEmptyHorizontal(target)) {
+      return true;
+    }
+    return false;
+  }
+}
